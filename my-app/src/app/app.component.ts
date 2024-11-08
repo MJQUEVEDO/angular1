7dc7c1255aeb0fcd768c1c6558ff6655ab71c87a
MJQUEVEDO <sendamadtrabajo@gmail.com>
2024-11-08 10:36:22 +0000

    mas camcios calendar

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,22 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { GridComponent } from './calendar/grid/grid.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, GridComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  name = 'María Jesús'
+  name = 'Pedro'
   isAutenticated = false
   data = [1,2,3,4,5,6]
   login(ev:Event){
     this.isAutenticated = true
   }
   changeData(){
-    this.data[3]=888;
-
+    this.data[3] = 88
   }
 }
